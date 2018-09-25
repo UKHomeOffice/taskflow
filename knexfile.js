dd4a763ccd3b786a09dc5bbf0a04942fc2cfdc71
Leonard Martin <leonard.martin@gmail.com>
2018-09-25 17:03:23 +0100

    Make env vars names consistent
    In /lib/db/migrate the variable name has `DATABASE` written out in full, where in the knexfile it's `DB`. Make those match each other.

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
       host: process.env.TASKFLOW_POSTGRES_HOST || 'localhost',
       user: process.env.TASKFLOW_POSTGRES_USER || 'taskflow-test',
       password: process.env.TASKFLOW_POSTGRES_PASSWORD,
-      database: process.env.TASKFLOW_POSTGRES_DB || 'taskflow-test',
+      database: process.env.TASKFLOW_POSTGRES_DATABASE || 'taskflow-test',
       port: process.env.TASKFLOW_POSTGRES_PORT || 5432
     }
   }
