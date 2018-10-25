394c5ef7786b691a9da0e94d9c3b7646e4b3942a
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2018-10-25 12:06:41 +0100

    use the correct username for the taskflow-test db

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
     client: 'postgres',
     connection: {
       host: process.env.TASKFLOW_POSTGRES_HOST || 'localhost',
-      user: process.env.TASKFLOW_POSTGRES_USER || 'taskflow-test',
+      user: process.env.TASKFLOW_POSTGRES_USER || 'postgres',
       password: process.env.TASKFLOW_POSTGRES_PASSWORD,
       database: process.env.TASKFLOW_POSTGRES_DATABASE || 'taskflow-test',
       port: process.env.TASKFLOW_POSTGRES_PORT || 5432
