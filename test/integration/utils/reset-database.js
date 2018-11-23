e51eaabfc35733b3e26a3f962796c9085b878bdc
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2018-11-23 11:13:16 +0000

    purge the activity log when resetting db

--- test/integration/utils/reset-database.js
+++ test/integration/utils/reset-database.js
@@ -1,11 +1,16 @@
+const { Model } = require('objection');
 const settings = require('../../../knexfile').test;
 const Case = require('../../../lib/db/case');
+const ActivityLog = require('../../../lib/models/activity-log');
 const Database = require('../../../lib/db');
 
 module.exports = () => {
   return Promise.resolve()
     .then(() => {
       const db = Database.connect(settings.connection);
-      return Case.query(db).delete();
+      Model.knex(db);
+      return Promise.resolve()
+        .then(() => ActivityLog.query().delete())
+        .then(() => Case.query(db).delete());
     });
 };
